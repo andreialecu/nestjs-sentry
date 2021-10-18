ce3acca68c2a6ba972a553ffa3bf8722211e61a5
Andrei Alecu <andreialecu@users.noreply.github.com>
2021-10-18 11:46:40 +0300

    refactor: remove/ignore dist and coverage

--- dist/injectDecoratoryFactory.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
-exports.makeInjectableDecorator = (token) => () => common_1.Inject(token);
-//# sourceMappingURL=injectDecoratoryFactory.js.map
